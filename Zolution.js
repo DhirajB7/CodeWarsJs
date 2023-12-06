4b1fdc211a061761336d16fa5186b94dd81660ab
DhirajB7 <dhiraj.basavaraju@gmail.com>
2023-12-06 11:46:58 +0530

    ++

--- Zolution.js
+++ Zolution.js
@@ -1,4 +1,38 @@
-const name = "Hello My name is Dhiraj!!____     903Dhsj".replace(/[^A-Za-z]/g,"")
-console.log(name)
-// const setName = new Set([...name.split('')].map(a=>a.toLowerCase()))
-// console.log(setName)
+//November 21, 2023 at 2:00:58.052 PM UTC+5:30
+
+const returnDateTimeStamp = () => {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const date = new Date();
+
+  const timeInAmPM = date
+    .toLocaleString("en-US", { hour: "numeric", hour12: true })
+    .split(" ");
+
+  return `${
+    monthNames[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()} at ${checkIfZeroShouldBePrefixed(
+    timeInAmPM[0]
+  )}:${checkIfZeroShouldBePrefixed(
+    date.getMinutes()
+  )}:${checkIfZeroShouldBePrefixed(date.getSeconds())}.${date.getMilliseconds()} ${timeInAmPM[1]}`;
+};
+
+const checkIfZeroShouldBePrefixed = (data = "") => {
+  return data.length === 1 ? "0" + data : data;
+};
+
+console.log(returnDateTimeStamp());
