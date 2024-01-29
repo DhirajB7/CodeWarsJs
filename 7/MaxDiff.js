07a1c933c32a3f90e87a32718b2d876c48dc1e00
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-01-29 22:47:55 +0530

    ++

--- 7/MaxDiff.js
+++ 7/MaxDiff.js
@@ -1,3 +1,4 @@
-const maxDiff = (list = []) => Math.max(...list) - Math.min(...list) || 0;
+const maxDiff = (list = []) =>
+  list.length > 1 ? Math.max(...list) - Math.min(...list) : 0;
 
 console.log(maxDiff([0, 1, 2, 3, 4, 5, 6]));
