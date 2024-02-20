cf19a19d27952d0143db97f84094c1a9ad5572a2
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-02-21 03:47:23 +0530

    ++

--- 5/MemoizedFibonacci.js
+++ 5/MemoizedFibonacci.js
@@ -2,7 +2,7 @@ function fibonacci(n, obj = {}) {
   if (n in obj) {
     return obj[n];
   }
-  if (n < 2) return 1;
+  if (n < 2) return n;
 
   obj[n] = fibonacci(n - 1, obj) + fibonacci(n - 2, obj);
 
